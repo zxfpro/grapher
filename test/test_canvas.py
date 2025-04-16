e39bbd0401243d47b7cc776626dab152a01f97fa
823042332@qq.com <823042332@qq.com>
2025-04-16 18:19:49 +0800

    ppp

--- test/test_canvas.py
+++ test/test_canvas.py
@@ -9,7 +9,7 @@ from grapher.canvas import Canvas,Color
 
 @pytest.fixture
 def canvas():
-    canva = Canvas(file_path='test/模拟资质认证.canvas')
+    canva = Canvas(file_path='/Users/zhaoxuefeng/GitHub/obsidian/工作/工程系统级设计/项目级别/数字人生/模拟资质认证/模拟资质认证.canvas')
     yield canva
     canva.to_file(file_path='test/模拟资质认证output.canvas')
 
@@ -17,5 +17,5 @@ def canvas():
 
 def test_canvas(canvas):
 
-    canvas.select_edges_by_text(key='处理')
+    canvas.select_edges_by_text(key='备案')
     canvas.select_by_color(key=Color.yellow,type='node')[4].color = Color.green.value
--- test/test_network.py
+++ test/test_network.py
@@ -1,15 +1,15 @@
-import sys
-import os
-# 将项目根目录添加到 Python 路径
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-import re
-import pytest
+# import sys
+# import os
+# # 将项目根目录添加到 Python 路径
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# import re
+# import pytest
 
-from grapher.network import NetworkXGraph
+# from grapher.network import NetworkXGraph
 
-@pytest.fixture
-def network():
-    canva = Canvas(file_path='test/模拟资质认证.canvas')
-    yield canva
-    canva.to_file(file_path='test/模拟资质认证output.canvas')
+# @pytest.fixture
+# def network():
+#     canva = Canvas(file_path='test/模拟资质认证.canvas')
+#     yield canva
+#     canva.to_file(file_path='test/模拟资质认证output.canvas')
 
